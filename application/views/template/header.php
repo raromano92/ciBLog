89290b52346c3da00cc024f1e396d928f60b8189
Robert Romano <bromano1992@gmail.com>
2022-10-02 22:41:30 -0400

    Configured and connected db and created posts model

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -14,8 +14,10 @@
       <div id="navbar">
         <ul class="nav navbar-nav">
           <li><a href="<?php echo base_url(); ?>">Home</a></li>
-          <li><a href="<?php echo base_url(); ?>/about">About</a></li>
+          <li><a href="<?php echo base_url(); ?>about">About</a></li>
         </ul>
       </div>
     </div>
   </nav>
+
+  <div class="container">
