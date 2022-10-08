b3c340867b5066f08d4ae3b8c236cd4fdcb92241
Robert Romano <bromano1992@gmail.com>
2022-10-08 03:20:22 -0400

    Added link to create a new post

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -19,6 +19,9 @@
           <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
 
         </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+
       </div>
     </div>
   </nav>
