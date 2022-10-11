223b298b31638d5c1ff0eb0af1f8c414250cc55d
Robert Romano <bromano1992@gmail.com>
2022-10-10 23:48:03 -0400

    Added category table in db and created drop down for categories on posts

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -26,6 +26,8 @@ class Posts extends CI_Controller {
   public function create() {
     $data['title'] = 'Create Post';
 
+    $data['categories'] = $this->post_model->get_categories();
+
     $this->form_validation->set_rules('title', 'Title', 'required');
     $this->form_validation->set_rules('body', 'Body', 'required');
 
